7830f067b913d69315cf81ad4a0f22d91fa5156c
Sean Canton <eighteyes@gmail.com>
2016-07-05 15:54:55 -0700

    Fix strict config fgiles

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var util = require('./helpers.js');
+cvar util = require('./helpers.js');
 var _ = require('lodash')
 
 var debug = require('debug')('config-helper')
@@ -37,67 +37,75 @@ function validate( config ){
     debug('Version:', config.configVersion);
     debug('Keywords:', config.keywords);
 
-    // set Datatype object defaults in case of array
-    if ( _.isArray(config.dataTypes) ){
-      config.dataTypes = _.fromPairs(
-        _.map( config.dataTypes, function(t){
-          // null means to interpret the data dynamically
-        return [t, null]
-      }))
-    }
+    //some apps just don't make data
+    if ( _.has(config, 'dataTypes')){
 
-    debug('DataTypes:', config.dataTypes);
+      // set Datatype object defaults in case of array
+      if ( _.isArray(config.dataTypes) ){
+        config.dataTypes = _.fromPairs(
+          _.map( config.dataTypes, function(t){
+            // null means to interpret the data dynamically
+            return [t, null]
+          }))
+        }
 
-    _.each(config.dataTypes, function(t, k){
-      if ( _.isObject(t) ){
-        // nested datatypes
-        _.each( t, function(ts, key) {
-          if ( !ts.match(dataTypeRegex) ){
-            console.error('Bad Data Type: ', key, ts);
+        debug('DataTypes:', config.dataTypes);
+
+        _.each(config.dataTypes, function(t, k){
+          if ( _.isObject(t) ){
+            // nested datatypes
+            _.each( t, function(ts, key) {
+              if ( !ts.match(dataTypeRegex) ){
+                console.error('Bad Data Type: ', key, ts);
+              }
+            })
+          } else {
+            if ( !_.isNull(t.match(dataTypeRegex)) && t.match(dataTypeRegex).length === 0 ) {
+              console.error('Bad Data Type: %s for %s', t, k)
+            }
           }
         })
-      } else {
-        if ( !_.isNull(t) && t.match(dataTypeRegex).length === 0 ) {
-          console.error('Bad Data Type: %s for %s', t, k)
-        }
-      }
-    })
+    }
 
     // debug(config.widgets);
     // check that all screens are widgets
-    var widgetList = Object.keys( config.widgets );
+    if ( _.has(config,'widgets') && _.has(config,'screens') ){
 
-    var screens = module.exports.screens = config.screens || {};
-    var widgets =module.exports.widgets = config.widgets || {};
-    var services =module.exports.services = config.services || {};
-    var filters =module.exports.filters = config.filters || {};
+      var widgetList = Object.keys( config.widgets );
 
-    module.exports.serviceNames = Object.keys( services );
-    module.exports.filterNames = Object.keys( filters );
-    module.exports.widgetNames = Object.keys( widgets );
-    module.exports.screenNames = Object.keys( screens );
+      var screens = module.exports.screens = config.screens || {};
+      var widgets =module.exports.widgets = config.widgets || {};
+      var services =module.exports.services = config.services || {};
+      var filters =module.exports.filters = config.filters || {};
 
-    var screenWidgetList = module.exports.screenWidgetList = _.flatten( _.map( screens, function ( v, k ) {
-      return _.flattenDeep( v );
-    } ) );
-    //
-    // debug(screenWidgetList);
-    var widgetDiff = _.difference( screenWidgetList, widgetList );
-    // debug(widgetDiff)
-    if ( widgetDiff.length > 0 ) {
-      throw new Error( 'Screens Missing Widget Definition: ' + widgetDiff.join( ', ' ) )
-    }
+      module.exports.serviceNames = Object.keys( services );
+      module.exports.filterNames = Object.keys( filters );
+      module.exports.widgetNames = Object.keys( widgets );
+      module.exports.screenNames = Object.keys( screens );
 
-    // log(widgets);
+      var screenWidgetList = module.exports.screenWidgetList = _.flatten( _.map( screens, function ( v, k ) {
+        return _.flattenDeep( v );
+      } ) );
+      //
+      // debug(screenWidgetList);
+      var widgetDiff = _.difference( screenWidgetList, widgetList );
+      // debug(widgetDiff)
+      if ( widgetDiff.length > 0 ) {
+        throw new Error( 'Screens Missing Widget Definition: ' + widgetDiff.join( ', ' ) )
+      }
 
-    var Widget = require('./Widget.js');
+      // log(widgets);
 
-    // check widgets for things
-    _.forIn( widgets, function ( w, name ) {
+      var Widget = require('./Widget.js');
+
+      // check widgets for things
+      _.forIn( widgets, function ( w, name ) {
 
         new Widget(name, w)
 
       })
+    }
+    //#end widget block
 
       config.validated = true;
       debug('=========== CONFIG VALIDATE ============='.blue)
