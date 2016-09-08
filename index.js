3ee97cb5895d22c8c3e49893fd219e96cdc8ec25
Sean Canton <eighteyes@gmail.com>
2016-09-07 23:57:00 -0700

    version

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ if (process.argv[2] === 'test'){
   console.log( require('util').inspect( config.validate(config.read(process.argv[3])), {depth: null}));
 }
 
+var v = JSON.parse(require('fs').readFileSync(__dirname + '/package.json')).version;
+
+debug( '📐  [ MATRIX ] App Config Helper v'.green + v.grey )
 
 module.exports = {
   config: config,
