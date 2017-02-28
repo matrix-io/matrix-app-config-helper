587656478bf6b00276673bfc4df2cb4810b2175c
Sean Canton <eighteyes@gmail.com>
2017-02-28 12:27:00 -0800

    Don't turn on debug

---
+++ HISTORY.md
@@ -0,0 +1,7 @@
+# HISTORY
+
+## 0.2.2
+- Don't turn on debug if CLI uses test as a keyword
+
+## 0.2.0
+- Added control validation to widget check
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require('colors');
 
-fs = require( 'fs' );
-yaml = require( 'js-yaml' )
-_ = require( 'lodash' )
+fs = require('fs');
+yaml = require('js-yaml')
+_ = require('lodash')
 var debug = require('debug')('app-config')
 log = console.log;
 
@@ -13,17 +13,19 @@ module.exports = {
   read: config.read,
   validate: config.validate,
   regex: {
-    string :/(string|str|s)/,
-    object :/(object|obj|o)/,
-    float :/(float|fl|f)/,
-    integer :/(integer|int|i)/,
-    boolean :/(b|bool|boolean)/,
+    string: /(string|str|s)/,
+    object: /(object|obj|o)/,
+    float: /(float|fl|f)/,
+    integer: /(integer|int|i)/,
+    boolean: /(b|bool|boolean)/,
   }
 }
 
-if ( process.argv[2] === 'test'){
+
+// this gets called by matrix cli sometimes, lets not turn on debug for no reason
+if (process.argv[2] === 'test' && process.argv[1].indexOf('matrix') === -1) {
   process.env['DEBUG'] = "*";
-  console.log( require('util').inspect( config.validate(config.read(process.argv[3])), {depth: null}));
+  console.log(require('util').inspect(config.validate(config.read(process.argv[3])), { depth: null }));
 }
 
 var msg;
@@ -31,23 +33,23 @@ var info = JSON.parse(require('fs').readFileSync(__dirname + '/package.json'));
 var currentVersion = info.version;
 require('https').get(
   'https://raw.githubusercontent.com/matrix-io/matrix-app-config-helper/master/package.json',
-function (res) {
-  var write = "";
-  res.on('data', function (c) {
-    write += c;
-  });
-  res.on('end', function (e) {
-    var remoteVersion = JSON.parse(write).version;
-    var msg = "";
-    if (currentVersion === remoteVersion) {
-      module.exports.current = true;
-      msg = '(current)'.grey;
-    } else {
-      module.exports.current = false;
-      msg = '(can upgrade to '.yellow+ remoteVersion +')'.yellow
-    }
-    debug( '📐  [ MATRIX ] App Config Helper v'.green + currentVersion.grey, msg )
-  });
-}).on('error', function (e) {
+  function(res) {
+    var write = "";
+    res.on('data', function(c) {
+      write += c;
+    });
+    res.on('end', function(e) {
+      var remoteVersion = JSON.parse(write).version;
+      var msg = "";
+      if (currentVersion === remoteVersion) {
+        module.exports.current = true;
+        msg = '(current)'.grey;
+      } else {
+        module.exports.current = false;
+        msg = '(can upgrade to '.yellow + remoteVersion + ')'.yellow
+      }
+      debug('📐  [ MATRIX ] App Config Helper v'.green + currentVersion.grey, msg)
+    });
+  }).on('error', function(e) {
   console.error('Upgrade Check Error: ', e)
 })
